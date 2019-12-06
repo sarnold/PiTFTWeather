b1e5fb74469784a944e0f62a6fd481e9a6d8fe22
Stephen Arnold <nerdboy@gentoo.org>
2019-12-06 09:58:53 -0800

    PiTFTWeather/PiTFTWeather.py: fixes and cleanup from testing
    Signed-off-by: Stephen Arnold <nerdboy@gentoo.org>

--- PiTFTWeather/PiTFTWeather.py
+++ PiTFTWeather/PiTFTWeather.py
@@ -1,10 +1,13 @@
-import os, syslog
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import os, sys, syslog
 import pygame
 import time
 import pywapi
 import string
 
-from daemon import *
+from daemon import Daemon as daemon
 
 # Weather Icons used with the following permissions:
 #
@@ -13,13 +16,14 @@ from daemon import *
 #
 # The icons are free to use for Non-Commercial use, but If you use want to use it with your art please credit me and put a link leading back to the icons DA page - http://vclouds.deviantart.com/gallery/#/d2ynulp
 #
-# *** Not to be used for commercial use without permission! 
+# *** Not to be used for commercial use without permission!
 # if you want to buy the icons for commercial use please send me a note - http://vclouds.deviantart.com/ ***
 
 installPath = "/opt/PiTFTWeather/"
 
 # location for Lincoln, UK on weather.com
-weatherDotComLocationCode = 'UKXX1087'
+LocationCode = '93455'
+
 # convert mph = kpd / kphToMph
 kphToMph = 1.60934400061
 
@@ -28,12 +32,12 @@ colourWhite = (255, 255, 255)
 colourBlack = (0, 0, 0)
 
 # update interval
-updateRate = 600 # seconds
+updateRate = 900 # seconds
 
-class pitft :
+class pitft:
     screen = None;
     colourBlack = (0, 0, 0)
-    
+
     def __init__(self):
         "Ininitializes a new pygame screen using the framebuffer"
         # Based on "Python GUI in Linux frame buffer"
@@ -43,7 +47,7 @@ class pitft :
             print "I'm running under X display = {0}".format(disp_no)
 
         os.putenv('SDL_FBDEV', '/dev/fb1')
-        
+
         # Select frame buffer driver
         # Make sure that SDL_VIDEODRIVER is set
         driver = 'fbcon'
@@ -54,13 +58,15 @@ class pitft :
         except pygame.error:
             print 'Driver: {0} failed.'.format(driver)
             exit(0)
-        
+
         size = (pygame.display.Info().current_w, pygame.display.Info().current_h)
+        print "Framebuffer Size: %d x %d" % (size[0], size[1])
         self.screen = pygame.display.set_mode(size, pygame.FULLSCREEN)
         # Clear the screen to start
-        self.screen.fill((0, 0, 0))        
+        self.screen.fill((0, 0, 0))
         # Initialise font support
         pygame.font.init()
+        pygame.mouse.set_visible(0)
         # Render the screen
         pygame.display.update()
 
@@ -70,7 +76,7 @@ class pitft :
 # Create an instance of the PyScope class
 mytft = pitft()
 
-pygame.mouse.set_visible(False)
+#pygame.mouse.set_visible(False)
 
 # set up the fonts
 # choose the font
@@ -85,56 +91,71 @@ class MyDaemon(daemon):
     def run(self):
             while True:
                 # retrieve data from weather.com
-                weather_com_result = pywapi.get_weather_from_weather_com(weatherDotComLocationCode)
-                
-                # extract current data for today
-                today = weather_com_result['forecasts'][0]['day_of_week'][0:3] + " " \
-                    + weather_com_result['forecasts'][0]['date'][4:] + " " \
-                    + weather_com_result['forecasts'][0]['date'][:3]
-                windSpeed = int(weather_com_result['current_conditions']['wind']['speed']) / kphToMph
-                currWind = "{:.0f}mph ".format(windSpeed) + weather_com_result['current_conditions']['wind']['text']
-                currTemp = weather_com_result['current_conditions']['temperature'] + u'\N{DEGREE SIGN}' + "C"
-                currPress = weather_com_result['current_conditions']['barometer']['reading'][:-3] + "mb"
+                weather_com_result = pywapi.get_weather_from_weather_com(LocationCode, units='imperial')
+
+                # extract current data and last data timestamp, reformat
+                ts = weather_com_result['current_conditions']['last_updated']
+                data_time = time.strptime(ts, "%m/%d/%y %I:%M %p %Z")
+                print(ts)
+                today = time.strftime("%d %b %H:%M", data_time)
+
+                test_windSpeed = weather_com_result['current_conditions']['wind']['speed']
+                if 'calm' in test_windSpeed:
+                    currWind = test_windSpeed
+                else:
+                    currWind = "{:.0f} mph ".format(int(weather_com_result['current_conditions']['wind']['speed'])) + \
+                        weather_com_result['current_conditions']['wind']['text']
+
+                currTemp = weather_com_result['current_conditions']['temperature'] + u'\N{DEGREE SIGN}' + " F"
+                currPress = weather_com_result['current_conditions']['barometer']['reading'] + " in Hg"
                 uv = "UV {}".format(weather_com_result['current_conditions']['uv']['text'])
-                humid = "Hum {}%".format(weather_com_result['current_conditions']['humidity'])
-                
+                humid = "RH {}%".format(weather_com_result['current_conditions']['humidity'])
+
                 # extract forecast data
                 forecastDays = {}
                 forecaseHighs = {}
                 forecaseLows = {}
                 forecastPrecips = {}
                 forecastWinds = {}
-            
+
+                # test data
+                test_WindSpd = {}
+
                 start = 0
-                try:
-                    test = float(weather_com_result['forecasts'][0]['day']['wind']['speed'])
-                except ValueError:
-                    start = 1
-            
+                #try:
+                #    test = float(weather_com_result['forecasts'][0]['day']['wind']['speed'])
+                #except ValueError:
+                #    start = 1
+
                 for i in range(start, 5):
-                
+
                     if not(weather_com_result['forecasts'][i]):
                         break
+
+                    test_WindSpd[i] = weather_com_result['forecasts'][i]['day']['wind']['speed']
+                    if 'calm' in test_WindSpd[i]:
+                        forecastWinds[i] = test_WindSpd[i]
+                    else:
+                        forecastWinds[i] = "{:.0f}".format(int(weather_com_result['forecasts'][i]['day']['wind']['speed'])) + \
+                            weather_com_result['forecasts'][i]['day']['wind']['text']
                     forecastDays[i] = weather_com_result['forecasts'][i]['day_of_week'][0:3]
-                    forecaseHighs[i] = weather_com_result['forecasts'][i]['high'] + u'\N{DEGREE SIGN}' + "C"
-                    forecaseLows[i] = weather_com_result['forecasts'][i]['low'] + u'\N{DEGREE SIGN}' + "C"
+                    forecaseHighs[i] = weather_com_result['forecasts'][i]['high'] + u'\N{DEGREE SIGN}' + "F"
+                    forecaseLows[i] = weather_com_result['forecasts'][i]['low'] + u'\N{DEGREE SIGN}' + "F"
                     forecastPrecips[i] = weather_com_result['forecasts'][i]['day']['chance_precip'] + "%"
-                    forecastWinds[i] = "{:.0f}".format(int(weather_com_result['forecasts'][i]['day']['wind']['speed'])  / kphToMph) + \
-                        weather_com_result['forecasts'][i]['day']['wind']['text']
-                    
+
                 # blank the screen
                 mytft.screen.fill(colourBlack)
-                
+
                 # Render the weather logo at 0,0
                 icon = installPath+ (weather_com_result['current_conditions']['icon']) + ".png"
                 logo = pygame.image.load(icon).convert()
                 mytft.screen.blit(logo, (0, 0))
-                
+
                 # set the anchor for the current weather data text
                 textAnchorX = 140
                 textAnchorY = 5
                 textYoffset = 20
-                
+
                 # add current weather data text artifacts to the screen
                 text_surface = font.render(today, True, colourWhite)
                 mytft.screen.blit(text_surface, (textAnchorX, textAnchorY))
@@ -153,11 +174,11 @@ class MyDaemon(daemon):
                 textAnchorY+=textYoffset
                 text_surface = font.render(humid, True, colourWhite)
                 mytft.screen.blit(text_surface, (textAnchorX, textAnchorY))
-            
+
                 # set X axis text anchor for the forecast text
                 textAnchorX = 0
                 textXoffset = 65
-                
+
                 # add each days forecast text
                 for i in forecastDays:
                     textAnchorY = 130
@@ -176,13 +197,13 @@ class MyDaemon(daemon):
                     text_surface = fontSm.render(forecastWinds[int(i)], True, colourWhite)
                     mytft.screen.blit(text_surface, (textAnchorX, textAnchorY))
                     textAnchorX+=textXoffset
-            
+
                 # refresh the screen with all the changes
                 pygame.display.update()
-                
+
                 # Wait
                 time.sleep(updateRate)
-        
+
 if __name__ == "__main__":
     daemon = MyDaemon('/tmp/PiTFTWeather.pid', stdout='/tmp/PiTFTWeather.log', stderr='/tmp/PiTFTWeatherErr.log')
     if len(sys.argv) == 2:
